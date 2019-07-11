680b480a433e4c140d63bddc8784d1df67d541fd
D3quien0 <35157795+D3quien0@users.noreply.github.com>
2019-07-10 20:43:18 -0400

    Update 99Bottles.py

--- 99Bottles.py
+++ 99Bottles.py
@@ -2,6 +2,7 @@ bottles = 99
 while bottles > 1:
     print(bottles,"Bottles of beer on the wall,", bottles, "bottles of beer")
     print("take one down, pass it around", bottles, "of beer on the wall")
+    print()
     bottles = bottles-1
     if bottles == 1:
         print(bottles, "bottle of beer on the wall, only one bottle of beer,")
